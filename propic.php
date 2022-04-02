cecd3300b0fdb09dbff1fca7c81a7e025c10ed42
Anandhu Narayanan <anandhuattuva114@gmail.com>
2022-04-02 13:26:28 +0530

    i

--- propic.php
+++ propic.php
@@ -11,6 +11,10 @@
             session_start();
             //var_dump($_FILES);
             //var_dump($_POST);
+            $id= $_SESSION['user_id'];
+            //echo $id;
+            //exit;
+            $s=0;
             if(isset($_POST['submit']))
             {
                 $uplFile = $_FILES['file'];
@@ -23,11 +27,21 @@
                 $uploaddir = 'images/';
                 $uplFileName = $uplFile['tmp_name'];
                 $newFilePath = $uploaddir.$newFileName;
-                if(move_uploaded_file($uplFileName, $newFilePath)){
-                    echo "upload successfully";
-                   
-                }else{
-                    echo "upload failed";
+                if(move_uploaded_file($uplFileName, $newFilePath))
+                {
+                    if(isset($_SESSION['user_id']))
+                    {
+                   $s=1;
+                    $qfbp= "insert into tbl_user (profile,status) values ('$newFileName','$s')";
+                    if(mysqli_query($con,$qfbp))
+                    {
+                        echo "upload successfully";
+                        header("Location:editprofile.php");
+                    }
+                }
+                    else{
+                        echo "upload failed";
+                }
                 }
             }
            
